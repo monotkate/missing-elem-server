efa9dd220f5da3640ff2eed9c1f3bd09117f8135
monotkate <mo.not.kate@gmail.com>
2016-04-15 14:05:28 -0700

    removed cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var express        =        require("express");
 var bodyParser     =        require("body-parser");
 var app            =        express();
-var cors           =        require("cors");
 
 //Here we are configuring express to use body-parser as middle-ware.
 //app.use(bodyParser.urlencoded({ extended: false }));
