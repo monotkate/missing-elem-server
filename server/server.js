9a0e971eb53446c9652dc5ccbc701f22b1ea2112
monotkate <mo.not.kate@gmail.com>
2016-04-15 13:54:32 -0700

    removed npm CORS, added enable

--- server/server.js
+++ server/server.js
@@ -6,7 +6,12 @@ var cors           =        require("cors");
 //Here we are configuring express to use body-parser as middle-ware.
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.set('port', (process.env.PORT || 8080));
 
